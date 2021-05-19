431f87b398eefec170a7b1258ab3d71edfd34f57
Artem Egorov <60894631+Nordinal@users.noreply.github.com>
2021-05-19 15:57:12 +0300

    190521

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs")
 const app = express();
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
@@ -11,10 +12,28 @@ const userShema = new Schema({
     password: String,
     isAdmin: Boolean,
 })
-
+const tagShema = new Schema({
+    tag: Array
+})
+const aboutUsShema = new Schema({
+    name: String,
+    desc: String,
+    descImg: String,
+    nameUrl: String,
+    url: String,
+    image: String
+})
+const messageSchema = new Schema({
+    name: String,
+    tel: String,
+    message: String,
+})
 const jsonParser = express.json();
 
 const User = mongoose.model("users", userShema);
+const Tags = mongoose.model("tags", tagShema);
+const AboutUs = mongoose.model("about", aboutUsShema);
+const Message = mongoose.model("messages", messageSchema);
 
 let allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -24,6 +43,18 @@ let allowCrossDomain = function(req, res, next) {
 }
 
 app.use(allowCrossDomain);
+
+app.post("/api/message", jsonParser, async function(req,res) {
+    try{
+        const {name, tel, message} = req.body
+        const newMessage = await Message.insertMany({name, tel, message})
+        res.json(true);
+    }
+    catch(e){
+        console.log(e)
+        res.json(false);
+    }
+})
 
 app.post("/api/user", jsonParser, async function(req,res) {
     try{
@@ -46,4 +77,35 @@ app.post("/api/user", jsonParser, async function(req,res) {
     }
 });
 
+app.get("/api/tag", jsonParser, async function(req, res) {
+    try{
+        const tags = await Tags.find()
+        res.json(tags[0]);
+    }
+    catch(e){
+        console.log(e);
+    }
+})
+app.get("/api/about", jsonParser, async function(req, res) {
+    try{
+        const about = await AboutUs.find();
+        res.json(about);
+    }
+    catch(e){
+        console.log(e);
+    }
+})
+
+app.get("/api/public/:id", async function(req, res){
+    try{
+        let file = fs.readFileSync(__dirname + "/public/" + req.params["id"]);
+        if(!!file){
+            res.write(file);
+        }
+    }
+    catch(e){
+        console.log(e);
+    }
+    res.end();
+})
 app.listen(5000);
